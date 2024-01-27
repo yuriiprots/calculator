6fb4614556b7a571ad284d492bea484738292c92
yuriiprots <uraprots7849@gmail.com>
2024-01-27 21:26:59 +0200

    update basic functions

--- script.js
+++ script.js
@@ -1,37 +1,69 @@
-let operations = ["+", "-", "*", "/"]
-let digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+// let operations = ["+", "-", "*", "/"];
+// let digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-function addNumbers(first_operand, second_operand) {
-  return first_operand + second_operand  
+let firstNumber;
+let operator;
+let secondNumber;
+let currentInput = "";
+
+function clearDisplay() {
+  currentInput = "";
+  updateDisplay();
 }
 
-function subtractNumbers(first_operand, second_operand) {
-  return first_operand - second_operand
+function appendNumber(number) {
+  currentInput += number;
+  updateDisplay();
 }
 
-
-function multiplyNumbers(first_operand, second_operand) {
-  return first_operand * second_operand
+function appendOperator(operator) {
+  currentInput += operator;
+  updateDisplay();
 }
 
-function divideNumbers(first_operand, second_operand) {
-  return first_operand / second_operand
-}
-
-function calculateResult(first_operand, second_operand, operator) {
-  if (operator === "+") {
-    return addNumbers(first_operand, second_operand)
-  } else if (operator === "-") {
-    return subtractNumbers(first_operand, second_operand)
-  } else if (operator === "*") {
-    return multiplyNumbers(first_operand, second_operand)
-  } else if (operator === "/") {
-    return divideNumbers(first_operand, second_operand)
+function updateDisplay() {
+  const displayElement = document.getElementById("display");
+  if (displayElement) {
+    displayElement.value = currentInput;
   }
+}
+
+function parser(expression) {
+  let first_operand;
+  let operator;
+  let second_operand;
+
   
 }
 
 
+function addNumbers(first_operand, second_operand) {
+  return first_operand + second_operand;
+}
+
+function subtractNumbers(first_operand, second_operand) {
+  return first_operand - second_operand;
+}
+
+function multiplyNumbers(first_operand, second_operand) {
+  return first_operand * second_operand;
+}
+
+function divideNumbers(first_operand, second_operand) {
+  return first_operand / second_operand;
+}
+
+console.log(addNumbers(5, 3));
+console.log(subtractNumbers(5, 3));
+console.log(multiplyNumbers(5, 3));
+console.log(divideNumbers(9, 3));
+
+function operate(first_operand, operator, second_operand) {
+  if (operator === "+") return addNumber(first_operand, second_operand);
+  if (operator === "-") return subtractNumbers(first_operand, second_operand);
+  if (operator === "*") return multiplyNumbers(first_operand, second_operand);
+  if (operator === "/") return divideNumbers(first_operand, second_operand);
+}
 
 // class Calculator {
 //   constructor() {
