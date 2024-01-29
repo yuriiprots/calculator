70d0b1ca83eb840f8a7003675a370d31f40f2156
yuriiprots <uraprots7849@gmail.com>
2024-01-29 16:55:46 +0200

    update the 'parser' and 'operate' functions

--- script.js
+++ script.js
@@ -1,75 +1,133 @@
-// let operations = ["+", "-", "*", "/"];
+const equalsBtn = document.getElementById("equalsBtn");
+const currentOperationDisplay = document.getElementById(
+  "currentOperationDisplay"
+);
+const lastOperationDisplay = document.getElementById("lastOperationDisplay");
+
+let operators = ["+", "-", "*", "/"];
 // let digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 let firstNumber;
 let operator;
 let secondNumber;
 let currentInput = "";
+let displayNumber = "";
+
+equalsBtn.addEventListener("click", () => parser(currentInput));
+// equalsBtn.addEventListener("click", () => toggleButton(equalsBtn));
+
+// function toggleButton(button) {
+//   button.disabled = !button.disabled;
+// }
 
 function clearDisplay() {
   currentInput = "";
-  updateDisplay();
+  currentOperationDisplay.innerHTML = "";
   lastOperationDisplay.innerHTML = "";
+  displayNumber = "";
 }
 
 function appendNumber(number) {
+  displayNumber += number;
   currentInput += number;
-  updateDisplay();
+  currentOperationDisplay.innerHTML = displayNumber;
 }
 
 function appendOperator(operator) {
   currentInput += operator;
-  updateDisplay();
+  lastOperationDisplay.innerHTML = currentInput;
+  displayNumber = "";
 }
 
-function updateDisplay() {
-  const currentOperationDisplay = document.getElementById(
-    "currentOperationDisplay"
-  );
-  const lastOperationDisplay = document.getElementById("lastOperationDisplay");
+// function updateDisplay(input) {
+//   if (currentOperationDisplay) {
+//     currentOperationDisplay.innerHTML = input;
+//   }
 
-  if (currentOperationDisplay) {
-    currentOperationDisplay.innerHTML = currentInput;
-  }
-  if (currentInput.match(/[+\-*/]/)) {
-    if (lastOperationDisplay) {
-      lastOperationDisplay.innerHTML = currentInput;
-    }
-  }
-}
-
-// function parser(expression) {
-//   let number = expression.match(/\d+/);
-//   let operator = expression.match(/[+\-*/]/);
+//   if (input.match(/[+\-*/]/)) {
+//     if (lastOperationDisplay) {
+//       lastOperationDisplay.innerHTML = input;
+//     }
+//   }
 // }
 
+function parser(expression) {
+  let first_operand;
+  let operator;
+  let second_operand;
+
+  operators.forEach((op) => {
+    if (expression.includes(op)) {
+      let index = expression.indexOf(op);
+      first_operand = Number(expression.slice(0, index));
+      operator = expression[index];
+      second_operand = Number(
+        expression.slice(index + 1, expression.length - 1)
+      );
+    }
+  });
+
+  console.log(first_operand, operator, second_operand);
+  operate(first_operand, operator, second_operand);
+
+  // for (let i = 0; i < expression.length; i++) {
+  //   if(expression[i] === "+" || expression[i] === "-" || expression[i] === "*" || expression[i] === "/") {
+  //     first_operand = Number(expression.slice(0, i));
+  //     operator = expression[i];
+  //     second_operand = Number(expression.slice(i + 1));
+  //   }
+  // }
+}
+
+function operate(first_operand, operator, second_operand) {
+  if (operator === "+") addNumbers(first_operand, second_operand);
+  if (operator === "-") subtractNumbers(first_operand, second_operand);
+  if (operator === "*") multiplyNumbers(first_operand, second_operand);
+  if (operator === "/") divideNumbers(first_operand, second_operand);
+}
+
 function addNumbers(first_operand, second_operand) {
-  return first_operand + second_operand;
+  let result = first_operand + second_operand;
+  lastOperationDisplay.innerHTML = currentInput;
+  currentOperationDisplay.innerHTML = result;
+  currentInput = result;
+  // let result = String(first_operand + second_operand);
+  // updateDisplay(result);
 }
 
 function subtractNumbers(first_operand, second_operand) {
-  return first_operand - second_operand;
+  let result = first_operand - second_operand;
+  lastOperationDisplay.innerHTML = currentInput;
+  currentOperationDisplay.innerHTML = result;
+  currentInput = result;
+
+  // let result = String(first_operand - second_operand);
+  // updateDisplay(result);
 }
 
 function multiplyNumbers(first_operand, second_operand) {
-  return first_operand * second_operand;
+  let result = first_operand * second_operand;
+  lastOperationDisplay.innerHTML = currentInput;
+  currentOperationDisplay.innerHTML = result;
+  currentInput = result;
+
+  // let result = String(first_operand * second_operand);
+  // updateDisplay(result);
 }
 
 function divideNumbers(first_operand, second_operand) {
-  return first_operand / second_operand;
+  let result = first_operand / second_operand;
+  lastOperationDisplay.innerHTML = currentInput;
+  currentOperationDisplay.innerHTML = result;
+  currentInput = result;
+  // let result = String(first_operand / second_operand);
+  // updateDisplay(result);
 }
 
-console.log(addNumbers(5, 3));
-console.log(subtractNumbers(5, 3));
-console.log(multiplyNumbers(5, 3));
-console.log(divideNumbers(9, 3));
-
-function operate(first_operand, operator, second_operand) {
-  if (operator === "+") return addNumber(first_operand, second_operand);
-  if (operator === "-") return subtractNumbers(first_operand, second_operand);
-  if (operator === "*") return multiplyNumbers(first_operand, second_operand);
-  if (operator === "/") return divideNumbers(first_operand, second_operand);
-}
+// console.log(addNumbers(5, 3));
+// console.log(subtractNumbers(5, 3));
+// console.log(multiplyNumbers(5, 3));
+// console.log(divideNumbers(9, 3));
 
 // class Calculator {
 //   constructor() {
